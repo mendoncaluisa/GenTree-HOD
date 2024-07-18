0f8e38e3ee53bfa8cf86db40cb6d8a9bf8a26c21
RafaelaMartins_ <rafaela.vieira@lynxprocess.com>
2024-07-18 18:05:51 -0300

    arvore

--- View/home_view.py
+++ View/home_view.py
@@ -1,6 +1,12 @@
+# home_view.py
 import dash_bootstrap_components as dbc
 from dash import dcc, Input, Output, State, html
+from dash_bootstrap_components._components.Container import Container
 import View.layout as lay
+
+PLOTLY_LOGO = "https://styles.redditmedia.com/t5_2vf7y/styles/communityIcon_9nbs2ss10b111.png"
+BACKGROUND_THEME = 'https://images.wallpapersden.com/image/download/house-of-dragon-dragonstone_bWxpbmaUmZqaraWkpJRobWllrWZuZmg.jpg'
+BACKGROUND = 'https://pbs.twimg.com/media/Ff2n8KhXgAAXgny?format=jpg&name=large'
 
 def build_tree(tree):
     if not tree:
@@ -17,10 +23,13 @@ def build_tree(tree):
     return children
 
 def create_content(data):
-    content = dbc.Row([html.Div([
+    content = dbc.Row([
+        html.Div([
             html.Div([
-        html.Ul(build_tree(data))
-    ], className="tree")], className="container m-0 p-1")],className="col-12")
+                html.Ul(build_tree(data))
+            ], className="tree")
+        ], className="container m-0 p-1")
+    ], className="col-12")
     return content
 
 def layout(data):
